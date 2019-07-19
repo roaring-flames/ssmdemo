0cada151a840b92ded008397199acfe9767860d2
jinliansheng <liansheng.jin@cyberway.net.cn>
2019-07-19 10:24:54 +0800

    ssm增删改查示例工程添加方法

--- src/main/java/com/example/DTO/InfoDTO.java
+++ src/main/java/com/example/DTO/InfoDTO.java
@@ -1,10 +1,13 @@
 package com.example.DTO;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 
-@Data
+@Setter
+@Getter
 public class InfoDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
